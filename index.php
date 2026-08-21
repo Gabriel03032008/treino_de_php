79b7f51feb0d97fa2d1621b25250e374cb6a136a
Gabriel Araújo de Freitas <freitas.araujo1@academico.ifrn.edu.br>
2026-08-21 16:22:31 -0300

    3 a 4

--- index.php
+++ index.php
@@ -21,4 +21,61 @@ echo "\n Fim da contagem.";
     }
  }
 
+ #Questão 3
+
+  $numero2 = 10;
+
+  for($i = 1; $i <= 10 ; $i++){
+
+    $soma = $i * $numero2;
+    
+    echo "\n", $numero2 ,"x", $i ,"=",$soma;
+
+  }
+
+  #Questão 4
+  $i = 0;
+  $soma2 = 0;
+  while($i <=100){
+    
+
+    $soma2 = $soma2 + $i;
+    $i++;
+  }
+
+  echo "\n","Soma total:", $soma2;
+
+  #Questão 5
+  $numero4 = 2;
+  if(($numero4 % 3) == 0 && ($numero4 % 5) == 0){
+    echo "\n multiplo de 3 e 5";
+  }elseif(($numero4 % 3) == 0){
+    echo "\n multiplo de 3";
+  }elseif(($numero4 % 5) == 0){
+    echo "\n multiplo de 5";
+  }else{
+    echo"\nnão é múltiplo de nenhum dos dois.\n";
+  }
+
+  #Questão 6
+
+  $a = 10;
+  $b = 10;
+  $c = 10;
+
+  $maior = $a;
+  if($maior < $b){
+
+    $maior = $b;
+    
+  }if($maior < $c){
+
+    $maior = $c;
+    
+  }else{
+    echo"os numeros são iguais\n";
+  }
+  echo "o numero ",$maior, " é o maior";
+  
+
 ?>
